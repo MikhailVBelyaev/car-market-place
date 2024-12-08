64fae7fefe91440f8cc1d3c408c5a3568ffcd800
Mihail Belyaev <mishelbelyaev@gmail.com>
2024-12-08 20:36:02 +0500

    add table favorites

---
+++ db/updates/V3__create_table_favorites.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS favorites (
+    favorite_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    car_id INT REFERENCES cars(car_id) ON DELETE CASCADE,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
