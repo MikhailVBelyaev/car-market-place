fe8a0da2b4a2afe4214ff439c0476b98b5d3bb45
Mihail Belyaev <mishelbelyaev@gmail.com>
2025-07-17 14:10:40 +0500

    modify for new api predict price v2

--- ml_api/main.py
+++ ml_api/main.py
@@ -11,10 +11,21 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 model = joblib.load(os.path.join("models", "car_price_model.pkl"))
+model_v2 = joblib.load(os.path.join("models", "car_price_model_v2.pkl"))
 
 class CarFeatures(BaseModel):
     year: int
     mileage: int
+
+class CarFeaturesV2(BaseModel):
+    year: int
+    mileage: int
+    brand: str
+    model: str
+    gear_type: str
+    color: str
+    fuel_type: str
+    body_type: str
 
 @app.get("/healthz")
 def health_check():
@@ -27,4 +38,12 @@ def predict(data: CarFeatures):
     prediction = model.predict(input_df)[0]
     logger.info(f"Received prediction request: {data}")
     logger.info(f"Prediction result: {prediction}")
+    return {"predicted_price": prediction}
+
+@app.post("/predict2")
+def predict_v2(data: CarFeaturesV2):
+    input_df = pd.DataFrame([data.dict()])
+    prediction = model_v2.predict(input_df)[0]
+    logger.info(f"Received prediction request (v2): {data}")
+    logger.info(f"Prediction result (v2): {prediction}")
     return {"predicted_price": prediction}
